55e264dc6fc3f5995c9db1c4182d73b221149fd7
xxxsjan <626653354@qq.com>
2024-05-13 18:55:52 +0800

    更新rollup配置，修复头部注释格式

--- rollup.config.js
+++ rollup.config.js
@@ -16,7 +16,7 @@ const defaultConfig = {
   output: {
     dir: './lib', // 输出目录
     format: 'es', // 输出格式, 可选值: amd, cjs, es, iife, umd
-    banner: '#!/usr/bin/env node', // 添加头部
+    banner: '#! /usr/bin/env node', // 添加头部
     // sourcemap: true, // 是否生成sourcemap, 默认为false
     // globals, // 外部依赖,
   },
